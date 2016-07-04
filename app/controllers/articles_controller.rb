30822f9ebf27fb0d58b3aa05d8553df835ef0b18
baothi <baothi246@gmail.com>
2016-07-04 11:45:51 +0000

    devise-mesages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, expect: [:index, :show]
   before_filter :set_article, only: [:show, :edit, :update, :destroy ]
   def index
     @articles = Article.all
