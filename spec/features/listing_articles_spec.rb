26a2a05d95aa173e3c384d2dec69ea38ad18ac43
baothi <baothi246@gmail.com>
2016-07-07 11:16:40 +0000

    restrict-access

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Listing Article" do
   before do 
     @article1 = Article.create(title: "The first article", body: "Body of first article")
     @article2 = Article.create(title: "The second article", body: "Body of second article")
-   end
+  end
    
    scenario "List all articles" do
     visit "/"
@@ -15,5 +15,6 @@ RSpec.feature "Listing Article" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
    end
 end
--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,7 +3,36 @@ require "rails_helper"
 RSpec.feature "Show an Article" do
 
   before do
-    @article = Article.create(title: "The first articles", body: "Body of first article")
+    @thi = User.create(email: "baothi246@gmail.com", password: "password")
+    @fred = User.create(email: "fred@gmail.com", password: "password")
+    @article = Article.create(title: "The first articles", body: "Body of first article", user: @thi)
+  end
+  
+  scenario "A non-signed in user does not see Edit or Delete links" do
+    visit "/"
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+    
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "A signed in owner sees both links" do
+    login_as(@thi)
   end
   
   scenario "Display individual article" do
